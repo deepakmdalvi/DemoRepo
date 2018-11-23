af726ee1d02b8e7ff868ab843152ae321c493a71
deepakmdalvi <deepakmdalvi@gmail.com>
2018-11-23 22:50:52 +0530

    First Git commit for DemoRepo selenium code.

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,11 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void homePageTest() {
+		System.out.println("We are in Home Page Test Class");
+	}
+}
---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void loginPageTest() {
+		System.out.println("We are in Login Page Test Class");
+	}
+
+}
